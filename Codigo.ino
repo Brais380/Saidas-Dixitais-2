a8a38726639937a8bccfae4344c14e0f8d9e7bc9
Brais380 <braisrey05@gmail.com>
2024-12-12 11:17:00 +0100

    Codigo.ino

--- Codigo.ino
+++ Codigo.ino
@@ -13,10 +13,10 @@ void setup()
 
 void loop()
 {
-  digitalWrite(12, LOW); //Apagado do led 12 e Encendio do led 13 
+  digitalWrite(12, LOW); //Apagado do pin 12 e Encendio do pin 13 
   digitalWrite(LED_BUILTIN, HIGH); 
   delay(10000); //Espera 10 segundo(s)
-  digitalWrite(LED_BUILTIN, LOW); //Apagado led 13 e Encendido led 12
+  digitalWrite(LED_BUILTIN, LOW); //Apagado pin 13 e Encendido pin 12
   digitalWrite(12, HIGH); 
   delay(4000); //Espera 4 segundos(s)
 }
